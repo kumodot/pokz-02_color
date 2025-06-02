5b4d23bf2e3f3c245c0ef7193699f09409f7c443
kumodot <84157396+kumodot@users.noreply.github.com>
2025-06-01 20:59:56 -0400

    Update App.tsx
    aDjust purple

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function createColorSystem() {
     GR: { name: "Green", rgb: [136, 196, 73] },
     IB: { name: "Ice Blue", rgb: [178, 240, 229] },
     BL: { name: "Blue", rgb: [69, 204, 245] },
-    PU: { name: "Purple", rgb: [135, 111, 204] },
+    PU: { name: "Purple", rgb: [148, 132, 187] },
     LG: { name: "Light Gray", rgb: [224, 224, 224] },
     MG: { name: "Medium Gray", rgb: [160, 160, 160] },
     DG: { name: "Dark Gray", rgb: [96, 96, 96] },
